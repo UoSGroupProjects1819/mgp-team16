aa755b8bced825f871f657987614087a448a703e
SionFW <SionFW@users.noreply.github.com>
2019-02-20 18:34:10 +0000

    AI Test
    AI Includes:
    Patrol
    Attack from Range & Retreat

---
+++ Unity Project/AI/AI Test/Assets/Scripts/Shooting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float speed;
+    public float StoppingDistance;
+    public float RetreatDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > StoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        else if (Vector2.Distance(transform.position, player.position) < StoppingDistance && Vector2.Distance(transform.position, player.position) > RetreatDistance)
+        {
+            transform.position = this.transform.position;
+        } else if (Vector2.Distance(transform.position, player.position) < RetreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if(timeBtwShots <= 0) {
+
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+    }
+}
