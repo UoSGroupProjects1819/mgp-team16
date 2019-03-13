b592075827f8f351e6d80783de762c8869f12c0b
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2019-03-13 14:38:58 +0000

    Deleted old Unity Builds and Merged into New Master Project
    Co-Authored-By: sionfw <sionfw@users.noreply.github.com>
    Co-Authored-By: Harry Wadman <hwadman@users.noreply.github.com>

---
+++ Unity Project/G16 Roguelike/G16 Roguelike/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed;
+    public float addRot;
+
+    void Start()
+    {
+        Destroy(gameObject, 1);
+    }
+
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * speed;
+        transform.Rotate(new Vector3(addRot, 0, 0));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Untagged")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Unity Project/G16 Roguelike/G16 Roguelike/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    [Header("Player Stats")]
+    public int maxHealth;
+    public int knockBack;
+    public int damageKnockBack;
+    public int rotationSpeed;
+    public float reloadSpeed;
+    public float invincibleTime;
+    public int bulletCount;
+    public float bulletSpread;
+
+    [Header("Read Variables")]
+    public int health;
+    public bool canShoot;
+    public bool invincible;
+    Quaternion lookQuaternion;
+    bool minus;
+
+    [Header("Init variables")]
+    public Rigidbody2D rB;
+    public Camera cam;
+    public GameObject bulletPrefab;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    void FixedUpdate()
+    {
+        //Init look at vectors
+        Vector3 mousePos = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        Vector3 transform2D = new Vector3(transform.position.x, transform.position.y, 0);
+        //translate look at vectors to a rotation
+        Vector3 lookVector = mousePos - transform2D;
+        float lookAngle = Mathf.Atan2(lookVector.y, lookVector.x) * Mathf.Rad2Deg;
+        lookQuaternion = Quaternion.AngleAxis(lookAngle, Vector3.forward);
+        //apply rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookQuaternion, Time.deltaTime * rotationSpeed);
+
+        if (Input.GetMouseButton(0))
+        {
+            if (canShoot)
+            {
+                StartCoroutine("Fire");
+            }
+        }
+
+        //TEST TEST TEST TEST TEST
+        if (Input.GetMouseButton(1))
+        {
+            if (!invincible)
+            {
+                StartCoroutine(Damage(mousePos));
+            }
+        }
+        //-------------------------
+    }
+
+    IEnumerator Fire()
+    {
+        canShoot = false;
+
+        for (int i = 0; i < bulletCount; i++)
+        {
+            minus = !minus;
+            if (!minus) {
+                GameObject tempBullet = Instantiate(bulletPrefab, transform.position, lookQuaternion);
+                tempBullet.transform.Rotate(0, 0, bulletSpread * i, Space.Self);
+            }
+            else
+            {
+                GameObject tempBullet = Instantiate(bulletPrefab, transform.position, lookQuaternion);
+                tempBullet.transform.Rotate(0, 0, bulletSpread * -i, Space.Self);
+            }
+        }
+
+        rB.AddForce(-transform.right * knockBack);
+        yield return new WaitForSeconds(reloadSpeed);
+        canShoot = true;
+    }
+
+    public IEnumerator Damage(Vector3 objPos)
+    {
+        if (!invincible)
+        {
+            if (health <= 1)
+            {
+                print("DEAD");
+                //KILL PLAYER
+            }
+            else
+            {
+                Vector3 dir = objPos - transform.position;
+                invincible = true;
+                health -= 1;
+                rB.AddForce(-dir * damageKnockBack);
+                yield return new WaitForSeconds(invincibleTime);
+                invincible = false;
+            }
+        }
+    }
+}
