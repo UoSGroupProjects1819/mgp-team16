77fbdcd0578555e492d9579a9eb9d52e240a8244
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2019-03-20 17:46:45 +0000

    Pushed updated Unity Build with Particle Effects
    Work completed by Sion
    
    Co-Authored-By: sionfw <sionfw@users.noreply.github.com>

---
+++ Unity Project/G16 Roguelike/Assets/Scripts/GunEffects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunEffects : MonoBehaviour
+{
+    public Transform MuzzleFlashPrefab;
+    public Transform Gun;
+
+    void Effect()
+    {
+        Transform clone = Instantiate(MuzzleFlashPrefab, Gun.position, Gun.rotation) as Transform;
+        clone.parent = Gun;
+        float size = Random.Range(0.5f, 1f);
+        clone.localScale = new Vector3(1, 1, 1);
+        Destroy(clone, 0.05f);
+    }
+
+}
