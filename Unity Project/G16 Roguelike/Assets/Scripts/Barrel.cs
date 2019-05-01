dda6ccdfe0be9e39c0618b4026a968d6bc7f8643
Fraser King <36007272+frasergeorgeking@users.noreply.github.com>
2019-05-01 15:28:21 +0100

    Pushed Updated Unity Build
    Co-Authored-By: Harry Wadman <hwadman@users.noreply.github.com>

---
+++ Unity Project/G16 Roguelike/Assets/Scripts/Barrel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+
+    bool hit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet")
+        {
+            StartCoroutine("Hit");
+        }
+    }
+
+    IEnumerator Hit()
+    {
+        if (!hit)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(2).gameObject.SetActive(true);
+            yield return new WaitForSeconds(1f);
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        hit = true;
+    }
+
+}
